b13f6d3c0a0cf8a115cd1cfa3db6d9799a35af2c
Resources - DTW <resource.dtw@gmail.com>
2025-03-20 14:46:25 +0530

    fix!:minor changes

--- src/screens/Gallery.js
+++ src/screens/Gallery.js
@@ -12,18 +12,20 @@ import Gimg7 from "../assests/Gimg7.png";
 import Gimg8 from "../assests/Gimg8.png";
 import Gimg9 from "../assests/Gimg9.png";
 import "../styles/Gallery.css";
+import Faq from "../components/Faq";
+import { WiDayCloudy } from "react-icons/wi";
 
 const images = [
-  { id: 1, src: Gimg1, alt: "Image 1" },
-  { id: 2, src: Gimg2, alt: "Image 2" },
-  { id: 3, src: Gimg3, alt: "Image 3" },
-  { id: 4, src: Gimg4, alt: "Image 4" },
-  { id: 5, src: Gimg5, alt: "Image 5" },
-  { id: 6, src: Gimg6, alt: "Image 6" },
-  { id: 7, src: Gimg7, alt: "Image 6" },
-  { id: 8, src: Gimg8, alt: "Image 6" },
-  { id: 8, src: Gimg7, alt: "Image 6" },
-  { id: 9, src: Gimg9, alt: "Image 6" },
+  { id: 1, src: Gimg1, alt: "Image 1", width: "346px", height: "478px" },
+  { id: 2, src: Gimg2, alt: "Image 2", width: "382px", height: "574px" },
+  { id: 3, src: Gimg3, alt: "Image 3", width: "346px", height: "478px" },
+  { id: 4, src: Gimg1, alt: "Image 4", width: "382px", height: "574px" },
+  { id: 5, src: Gimg2, alt: "Image 5", width: "346px", height: "478px" },
+  { id: 6, src: Gimg3, alt: "Image 6", width: "382px", height: "574px" },
+  { id: 7, src: Gimg1, alt: "Image 7", width: "346px", height: "478px" },
+  { id: 8, src: Gimg2, alt: "Image 8", width: "382px", height: "574px" },
+  { id: 8, src: Gimg3, alt: "Image 9", width: "346px", height: "478px" },
+  { id: 9, src: Gimg1, alt: "Image 10", width: "382px", height: "574px" },
 ];
 
 export default function Gallery() {
@@ -31,50 +33,17 @@ export default function Gallery() {
     <div>
       <Header />
       <div className="hero-section">
-        <div className="Glayer1">
-          <svg
-            width="37"
-            height="42"
-            viewBox="0 0 37 42"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <circle cx="29" cy="8" r="8" fill="#473FE1" />
-            <circle cx="16.5" cy="31.5" r="10.5" fill="#473FE1" />
-            <circle cx="6" cy="14" r="6" fill="#473FE1" />
-          </svg>
-        </div>
-        <div className="Glayer2">
-          <svg
-            width="37"
-            height="42"
-            viewBox="0 0 37 42"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <circle
-              cx="8"
-              cy="34"
-              r="8"
-              transform="rotate(-180 8 34)"
-              fill="#473FE1"
-            />
-            <circle
-              cx="20.5"
-              cy="10.5"
-              r="10.5"
-              transform="rotate(-180 20.5 10.5)"
-              fill="#473FE1"
-            />
-            <circle
-              cx="31"
-              cy="28"
-              r="6"
-              transform="rotate(-180 31 28)"
-              fill="#473FE1"
-            />
-          </svg>
-        </div>
+        <p
+          style={{
+            fontSize: 18,
+            fontWeight: 600,
+            letterSpacing: "3px",
+            color: " #F461A6",
+            textAlign: "center",
+          }}
+        >
+          LEADING TRAINING CENTER
+        </p>
         <p className="hero-title">
           Explore Our Academy – A Glimpse into Learning & Innovation!
         </p>
@@ -83,53 +52,19 @@ export default function Gallery() {
           creativity. From hands-on coding sessions to interactive workshops,
           experience what makes our academy unique.
         </p>
+        <Button
+          className="contact-btn"
+          style={{
+            backgroundColor: "#473FE1",
+            borderRadius: "12px",
+            color: "#FFFFFF",
+          }}
+        >
+          Courses
+        </Button>
       </div>
 
       <div className="service-container">
-        <div className="Glayer3">
-          <svg
-            width="37"
-            height="42"
-            viewBox="0 0 37 42"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <circle cx="29" cy="8" r="8" fill="#473FE1" />
-            <circle cx="16.5" cy="31.5" r="10.5" fill="#473FE1" />
-            <circle cx="6" cy="14" r="6" fill="#473FE1" />
-          </svg>
-        </div>
-        <div className="Glayer4">
-          <svg
-            width="37"
-            height="42"
-            viewBox="0 0 37 42"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <circle
-              cx="8"
-              cy="34"
-              r="8"
-              transform="rotate(-180 8 34)"
-              fill="#473FE1"
-            />
-            <circle
-              cx="20.5"
-              cy="10.5"
-              r="10.5"
-              transform="rotate(-180 20.5 10.5)"
-              fill="#473FE1"
-            />
-            <circle
-              cx="31"
-              cy="28"
-              r="6"
-              transform="rotate(-180 31 28)"
-              fill="#473FE1"
-            />
-          </svg>
-        </div>
         <div className="services-text">
           <p className="services-title">Moments Of Joy Through Work</p>
           <p className="services-sub-title">
@@ -139,76 +74,71 @@ export default function Gallery() {
         </div>
       </div>
 
-      <div className="image-grid">
-        {images.map((image) => (
-          <div key={image.id} className="image-container">
-            <img src={image.src} alt={image.alt} />
-          </div>
-        ))}
+      <div
+        style={{
+          display: "flex",
+          gap: 30,
+          overflowX: "scroll",
+          width: "100%",
+          scrollbarWidth: "none",
+          msOverflowStyle: "none",
+          marginBottom: "10%",
+        }}
+        className="image-slider"
+      >
+        <div className="image-track">
+          {[...images, ...images].map((image, index) => (
+            <div
+              key={image.id}
+              style={{
+                flex: "0 0 auto",
+                marginTop: index % 2 === 0 ? "2%" : "0px",
+              }}
+              // className="hover-rotate"
+            >
+              <img
+                src={image.src}
+                alt={image.alt}
+                className="animated-image"
+                style={{
+                  width: image.width,
+                  height: image.height,
+                }}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="upcoming-batch">
+        <div className="services-text">
+          <p className="faq-title">FAQ</p>
+          <p className="faq-sub-title">
+            Find answers to common questions about our courses, enrollment, and
+            more.
+          </p>
+          <Faq />
+        </div>
       </div>
 
       <div className="cta-container">
-        <div className="Glayer20">
-          <svg
-            width="37"
-            height="42"
-            viewBox="0 0 37 42"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <circle cx="29" cy="8" r="8" fill="white" />
-            <circle cx="16.5" cy="31.5" r="10.5" fill="white" />
-            <circle cx="6" cy="14" r="6" fill="white" />
-          </svg>
-        </div>
-        <div className="Glayer21">
-          <svg
-            width="37"
-            height="42"
-            viewBox="0 0 37 42"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <circle
-              cx="8"
-              cy="34"
-              r="8"
-              transform="rotate(-180 8 34)"
-              fill="white"
-            />
-            <circle
-              cx="20.5"
-              cy="10.5"
-              r="10.5"
-              transform="rotate(-180 20.5 10.5)"
-              fill="white"
-            />
-            <circle
-              cx="31"
-              cy="28"
-              r="6"
-              transform="rotate(-180 31 28)"
-              fill="white"
-            />
-          </svg>
-        </div>
-        <p className="cta-title">Kickstart Your Career In Tech Today!</p>
+        <p className="cta-title">Kickstart Your Career in Tech Today!</p>
         <p className="cta-subtitle">
           Learn Full Stack Engineering, Business Analysis, and Data Science with
-          Industry Experts.
+          Industry Experts. Learn Full Stack Engineering, Business Analysis, and
+          Data Science with Industry Experts.
         </p>
         <Button
           className="enroll-button"
           style={{
             backgroundColor: "#FFFFFF",
-            borderRadius: "100px",
+            borderRadius: "12px",
             color: "#473FE1",
           }}
         >
           Enrol now
         </Button>
       </div>
-
       <Footer />
     </div>
   );
